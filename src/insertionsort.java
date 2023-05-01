2bc19f1354279e442a33cbe0bcea7b1bf9d612af
Raktim Bar <91616950+raktimbar100@users.noreply.github.com>
2023-05-01 21:14:15 +0530

    Create insertionsort.java

---
+++ src/insertionsort.java
@@ -0,0 +1,23 @@
+static void insertion(int[] arr)
+    {
+        for(int i=0;i<arr.length-1;i++)
+        {
+            for(int j=i+1;j>0;j--)
+            {
+                if(arr[j]<arr[j-1])
+                {
+                    swap(arr,j,j-1);
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
+
+    static void swap(int[] arr,int start,int end)
+    {
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+    }
